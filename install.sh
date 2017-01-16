003eb741fc927561034d3bbbf3826c6710975657
Manuel <mvargas@xpectrumtech.com>
2017-01-16 16:51:10 -0500

    Fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 docker rm -fv despachalo-proxy-docker
 docker build -t despachalo-proxy-docker .
-docker run -d --privileged=true --name despachalo-ssl-proxy -v /etc/letsencrypt:/etc/letsencrypt --link despachalo-rest-api-8084 -p 80:80 -p 443:443 despachalo-proxy-docker
-# -e CERTS=despachalo.xpectrumtech.com -e EMAIL=despachalo@xpectrumtech.com
+docker run -d --privileged=true --name despachalo-ssl-proxy -e CERTS=despachalo.xpectrumtech.com -v /etc/letsencrypt:/etc/letsencrypt --link despachalo-rest-api-8084 -p 80:80 -p 443:443 despachalo-proxy-docker
+# -e EMAIL=despachalo@xpectrumtech.com
